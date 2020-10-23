7e213de09eb1480f19a614a12bea579e82c493ef
Unknown <CIDJEY1991@mail.ru>
2020-10-23 22:15:55 +0300

    get tests to run

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 let argv = require('yargs/yargs')(process.argv.slice(2))
 	.usage('Usage: $0 [options]')
-	.demandOption(['jira_url', 'username', 'password', 'project'])
+	.demandOption(['jira_url', 'username', 'password', 'project', 'cycle', 'testo_project_dir'])
 	.describe('jira_url', 'target JIRA url (e.g. http://www.my_host.ru/jira')
 	.describe('username', 'JIRA login')
 	.describe('password', 'JIRA password')
 	.describe('project', 'JIRA project name')
+	.describe('cycle', 'TJ4M cycle to run by Testo')
+	.describe('testo_project_dir', 'path to the dir with Testo tests')
 	.argv;
 
 
@@ -21,7 +25,21 @@ let jira_rest_endpoint = argv.jira_url + "/rest/atm/1.0/"
 async function main() {
 	try {
 		const cycles = await axios.get(jira_rest_endpoint + `testrun/search?query=projectKey = "${argv.project}"`, {auth: credentials});
-		console.log(cycles.data)
+
+		let tests_to_run = []
+
+		for (let cycle of cycles.data) {
+			if (cycle.key == argv.cycle) {
+				for (let item of cycle.items) {
+					tests_to_run.push(item.testCaseKey)
+				}
+				break;
+			}
+		}
+
+		console.log(tests_to_run)
+
+
 	} catch (error) {
 		console.error("ERROR")
 		console.error(error)
