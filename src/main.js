1b5637f31f6b440ee8588b4cb0a6a0742c83a1dd
Unknown <CIDJEY1991@mail.ru>
2020-10-24 21:28:03 +0300

    basic post results

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const child_process = require('child_process')
 
 let argv = require('yargs/yargs')(process.argv.slice(2))
 	.usage('Usage: $0 [options]')
@@ -28,8 +29,6 @@ let report_folder = '/tmp/testo_tm4j_report_folder'
 if (!fs.existsSync(report_folder)){
 	fs.mkdirSync(report_folder);
 }
-
-console.log(argv.param)
 
 let params = []
 
@@ -62,6 +61,38 @@ async function walk(dir, basenames_to_match) {
 	return result
 }
 
+function testo_run(args) {
+	return new Promise(function(resolve, reject) {
+		let p = child_process.spawn('testo', args)
+		let stdout = ''
+		let stderr = ''
+		p.stdout.on('data', data => stdout += data)
+		p.stderr.on('data', data => stderr += data)
+		p.on('exit', code => {
+			if (code) {
+				reject(stderr)
+			} else {
+				resolve(stdout)
+			}
+		})
+		p.on('error', error => reject(error))
+	})
+}
+
+const deleteFolderRecursive = function(folder) {
+	if (fs.existsSync(folder)) {
+		fs.readdirSync(folder).forEach((file, index) => {
+			const curPath = path.join(folder, file);
+			if (fs.lstatSync(curPath).isDirectory()) { // recurse
+				deleteFolderRecursive(curPath);
+			} else { // delete file
+				fs.unlinkSync(curPath);
+			}
+		});
+		fs.rmdirSync(folder);
+	}
+};
+
 async function main() {
 	try {
 
@@ -85,20 +116,66 @@ async function main() {
 		console.log(files_to_run)
 
 		for (file_to_run of files_to_run) {
-			let report_test_folder = report_folder + '/' + path.basename(file_to_run).name
-			let testo_run_command = 'testo run ' + file_to_run
-				+ ' --assume_yes'
-				+ ' --report_folder ' + report_test_folder
-				+ ' --report_logs'
-				+ ' --report_screenshots'
+			let test_case_key = path.parse(file_to_run).name
+			let report_test_folder = path.join(report_folder, test_case_key)
+
+			deleteFolderRecursive(report_test_folder)
+
+			let testo_args = []
+
+			testo_args.push('run')
+			testo_args.push(file_to_run)
+			testo_args.push('--assume_yes')
+			testo_args.push('--report_folder')
+			testo_args.push(report_test_folder)
+			testo_args.push('--report_logs')
+			testo_args.push('--report_screenshots')
+
+			if (argv.license) {
+				testo_args.push('--license')
+				testo_args.push(argv.license)
+			}
 
 			for (param of params) {
-				testo_run_command += ` --param ${param.name} ${param.value}`
+				testo_args.push('--param')
+				testo_args.push(param.name)
+				testo_args.push(param.value)
 			}
-			console.log(testo_run_command)
+
+			testo_args.push('--invalidate')
+			testo_args.push('some_test_2')
+
+			let output = await testo_run(testo_args)
+			console.log(output)
+
+			let jira_test_run_report = []
+
+			let testo_report = JSON.parse(fs.readFileSync(report_test_folder + '/report.json'))
+
+			console.log(testo_report)
+
+			let current_index = 0
+
+			jira_test_run_report.push({
+				status: 'Pass',
+				testCaseKey: test_case_key,
+				executionTime: Math.abs(Date.parse(testo_report.stop_timestamp) - Date.parse(testo_report.start_timestamp)),
+				executionDate: testo_report.start_timestamp,
+				executedBy: argv.username,
+
+				scriptResults: [
+					{
+						index: 0,
+						status: 'Pass',
+						comment: output
+					}
+				]
+			})
+
+			let post_reponse = await axios.post(jira_rest_endpoint + `testrun/${argv.cycle}/testresults/`, jira_test_run_report, {auth: credentials})
+
+			console.log(post_reponse)
 		}
-
-
 	} catch (error) {
 		console.error("ERROR")
 		console.error(error)
@@ -108,3 +185,12 @@ async function main() {
 
 main()
 
+
+async function tmp() {
+const cycles = await axios.get(jira_rest_endpoint + `testrun/SAMPLE-C6/testresults`, {auth: credentials});
+console.log (cycles.data)
+
+}
+
+
+//tmp()
