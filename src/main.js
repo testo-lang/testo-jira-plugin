a4af26907fd8222bf3bd747f8bb49bac92573c39
Unknown <CIDJEY1991@mail.ru>
2020-10-24 12:36:11 +0300

    forming testo command

--- src/main.js
+++ src/main.js
@@ -12,12 +12,32 @@ let argv = require('yargs/yargs')(process.argv.slice(2))
 	.describe('project', 'JIRA project name')
 	.describe('cycle', 'TJ4M cycle to run by Testo')
 	.describe('testo_project_dir', 'path to the dir with Testo tests')
+	.describe('param', 'param to pass into Testo script')
+	.describe('license', 'license file to pass into Testo')
+	.nargs('param', 2)
 	.argv;
 
 
 let credentials = {
 	username: argv.username,
 	password: argv.password
+}
+
+let report_folder = '/tmp/testo_tm4j_report_folder'
+
+if (!fs.existsSync(report_folder)){
+	fs.mkdirSync(report_folder);
+}
+
+console.log(argv.param)
+
+let params = []
+
+for (let i = 0; i < argv.param.length; i++) {
+	params.push ({
+		name: argv.param[i],
+		value: argv.param[++i]
+	})
 }
 
 let jira_rest_endpoint = argv.jira_url + "/rest/atm/1.0/"
@@ -32,7 +52,7 @@ async function walk(dir, basenames_to_match) {
 			result = result.concat(await walk(file_path, basename_to_match))
 		} else {
 			for (basename of basenames_to_match) {
-				if (path.basename(file_path) == basename) {
+				if (path.basename(file_path) == basename + '.testo') {
 					result.push(file_path)
 				}
 			}
@@ -44,6 +64,7 @@ async function walk(dir, basenames_to_match) {
 
 async function main() {
 	try {
+
 		const cycles = await axios.get(jira_rest_endpoint + `testrun/search?query=projectKey = "${argv.project}"`, {auth: credentials});
 
 		let tests_to_run = []
@@ -61,7 +82,22 @@ async function main() {
 
 		let files_to_run = await walk(argv.testo_project_dir, tests_to_run)
 
-		console.log(files)
+		console.log(files_to_run)
+
+		for (file_to_run of files_to_run) {
+			let report_test_folder = report_folder + '/' + path.basename(file_to_run).name
+			let testo_run_command = 'testo run ' + file_to_run
+				+ ' --assume_yes'
+				+ ' --report_folder ' + report_test_folder
+				+ ' --report_logs'
+				+ ' --report_screenshots'
+
+			for (param of params) {
+				testo_run_command += ` --param ${param.name} ${param.value}`
+			}
+			console.log(testo_run_command)
+		}
+
 
 	} catch (error) {
 		console.error("ERROR")
